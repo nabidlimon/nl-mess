df1bd83ddfc42a13c6e0a2d2369a92abca3412cd
Nabid Ahammed Limon <dynamicnabid@gmail.com>
2026-07-02 07:24:22 +0600

    Remove all Android and Capacitor related files and CI workflows

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.messtracker.app',
-  appName: 'mess-tracker',
-  webDir: 'dist'
-};
-
-export default config;
